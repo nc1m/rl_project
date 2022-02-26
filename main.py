c48e1289795630f744cbc2c394b1e64c27618cd5
Tri Hiep Bui <trihiepbui@MBP-von-Tri.multi.box>
2022-02-26 13:49:57 +0100

    args parser

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import sys
 from gym import Env
 from src import env
-
-EPISODES = 200
-EPISODE_MAX_STEP = 100
+import argparse
 
 
-def main():
+ENVIROMENT_CHOICES = ['CartPole-v0', 'HumanoidStandup-v2']
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="""
+TODO Description""", formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-e', '--env',
+                        default='CartPole-v0',
+                        type=str,
+                        choices=ENVIROMENT_CHOICES,
+                        help='Set enviroment (see description above for details). Possible values are: '+', '.join(ENVIROMENT_CHOICES),
+                        metavar='env')
+    parser.add_argument('-s', '--seed', default=42, type=int,
+                        help='Sets the seed for this experiment and consequently the random sample shown.')
+    parser.add_argument('-eps', '--episodes', default=200, type=int,
+                        help='The number of episode for the enviroment.')
+    parser.add_argument('-mepss', '--maxEpisodeSteps', default=100, type=int,
+                        help='The maximum episode steps in a episode')
+    parser.add_argument('--no_cuda', action='store_true', help='Set this if cuda is availbable, but you do NOT want to use it.')
+    
+    return parser.parse_args()
+
+def main(args):
     
     agent = None
-    enviroment = env.Enviroment('CartPole-v0')
-    enviroment.play(EPISODES, EPISODE_MAX_STEP)
+    enviroment = env.Enviroment(args.env)
+    enviroment.play(args.episodes, args.maxEpisodeSteps)
 
 
 if __name__ == '__main__':
-    sys.exit(main())  
+    args = parse_args()
+    sys.exit(main(args))  
