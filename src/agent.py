2c51a37b7fe3e96bf4c55c4ac165a7cf5f53a000
Tri Hiep Bui <trihiepbui@MBP-von-Tri.multi.box>
2022-02-27 18:11:03 +0100

    fc agent

--- src/agent.py
+++ src/agent.py
@@ -1,11 +1,38 @@
 from torch import nn
+from collections import deque
+
+class FCDQN(nn.Module):
+    def __init__(self, OBS_SPACE, ACTION_SPACE, LAYERS, DEPTH, BIAS):
+        super(FCDQN, self).__init__()
+        
+        self.layers = LAYERS
+        self.input_layer = nn.Linear(OBS_SPACE, DEPTH, bias = BIAS)
+        self.hidden_layer = nn.Linear(DEPTH, DEPTH, bias = BIAS)
+        self.output_layer = nn.Linear(DEPTH, ACTION_SPACE, bias = BIAS)
+        self.ReLU = nn.ReLU()
+        
+    def forward(self, x):
+        
+        x = self.input_layer(x)
+        x = self.ReLU(x)
+        for _ in self.layers:
+            x = self.hidden_layer(x)
+            x = self.ReLU(x)
+        
+        return self.output_layer(x)
+        
+        
 
 
-class SPRAgent():
-    def __init__(self) -> None:
-        pass
+class SPRAgent(FCDQN):
+    def __init__(self, buffersize) -> None:
+        self.buffer = deque([], buffersize)
     
     def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass
+    
+    def forward(self, x):
+        super().forward(x)
         pass
     
     def train():
