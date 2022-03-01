76592e3e59f57e7466593553467d35af312960a0
nc1m <userw@debian-desktop>
2022-03-01 16:24:24 +0100

    removed torch conversion and normalization. code format.

--- src/cartpole_wrapper.py
+++ src/cartpole_wrapper.py
@@ -1,33 +1,27 @@
 import time
 import gym
 import numpy as np
-from collections import namedtuple
-
-import torch
-from IPython.display import clear_output
-import torchvision.transforms as T
 from gym import spaces
-
-from matplotlib import pyplot as plt
-
 
 
 def get_screen(env):
     ''' Extract one step of the simulation.'''
     screen = env.render(mode='rgb_array')
-    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255.
-    return torch.from_numpy(screen)
+    screen = np.ascontiguousarray(screen, dtype=np.float32)
+    return screen
+
 
 class ObservationWrapper(gym.ObservationWrapper):
     def __init__(self, env) -> None:
         super().__init__(env)
-        self.observation_space = spaces.Box(0,255,(3,400,600),np.uint8)
+        self.observation_space = spaces.Box(0, 255, (3, 400, 600), np.uint8)
 
     def observation(self, observation):
         return get_screen(self.env)
 
 
 def main():
+    from matplotlib import pyplot as plt
     env = gym.envs.make("CartPole-v1")
     env.reset()
     screen = env.render(mode='rgb_array')
@@ -41,11 +35,10 @@ def main():
 
     for _ in range(10):
         env.render()
-        obs, reward,_,_ =env.step(env.action_space.sample())
+        obs, reward, _, _ = env.step(env.action_space.sample())
         plt.imshow(obs)
         plt.show()
         time.sleep(1)
-
 
     env.close()
 
