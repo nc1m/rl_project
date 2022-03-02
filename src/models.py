de24a162dd8be33db7f42036dff0d8cdc5b54f40
Tri Hiep Bui <hiepbui945@gmail.com>
2022-03-02 22:28:46 +0100

    forward

--- src/models.py
+++ src/models.py
@@ -1,20 +1,22 @@
 from torch import nn
 import torch
 import numpy as np
-import copy
 import utils
+import torch.nn.functional as F
 
-#TODO
 class SPRModel(nn.Module):
     
-    def __init__(self, input_size, output_size, time_offset, image_size, image_shape, dqn_hidden_size):
+    def __init__(self, input_size, output_size, time_offset, image_size, image_shape, dqn_hidden_size, jumps, model_rl):
+        
         self.input_size = input_size
         self.output_size = output_size
         self.time_offset = time_offset
         self.dqn_hidden_size = dqn_hidden_size
-        
+        self.jumps = jumps
+        self.model_rl = model_rl
         f, c = image_shape[:2]
         in_channels = np.prod(image_shape[:2])
+        #Online Encoder
         self.o_encoder = OnlineEncoder(in_channels,
                                            out_channels = [32, 64, 64],
                                            kernel_sizes = [8, 4, 3],
@@ -30,20 +32,118 @@ class SPRModel(nn.Module):
         self.pixels = fake_output.shape[-1]*fake_output.shape[-2]
         print("Spatial latent size is {}".format(fake_output.shape[1:]))
         
-        self.head = MLPOnlineProjectionHead(self.hidden_size, output_size, self.dqn_hidden_size, self.pixels)
+        #Q-Learning-Head
+        self.head = MLPQHead(self.hidden_size, output_size, self.dqn_hidden_size, self.pixels)
         
-        self.transition = TransitionModel(channels=self.hidden_size, 
-                                            num_actions=output_size,
-                                            pixels = self.pixels,
-                                            hidden_size=self.hidden_size,
-                                            limit = 1)
+        if self.jumps > 0:
+            #Transition Model, falls Parameter jumps (k) > 0
+            self.transition = TransitionModel(channels=self.hidden_size, 
+                                                num_actions=output_size,
+                                                pixels = self.pixels,
+                                                hidden_size=self.hidden_size,
+                                                limit = 1)
         
+        #Target Encoder
         self.target_encoder = utils.EMA(model=self.o_encoder, decay=0.99)
-        #TODO: Q-LEARNING HEAD AND TARGET PROJECTION
+        
+        #Online Projection Head
+        self.online_projection = nn.Sequential(nn.Linear(self.hidden_size, 
+                                                            2*self.hidden_size),
+                                                            nn.BatchNorm1d(2*self.hidden_size),
+                                                            nn.ReLU(),
+                                                            nn.Linear(2*self.hidden_size,
+                                                            self.hidden_size))
+        #Target Projection Head
+        self.target_projection = self.online_projection
+        
+    def head_forward(self, latent):
+        return self.head(latent)
     
-class MLPOnlineProjectionHead(nn.Module):
+    def o_encoder_forward(self, img):
+        return self.o_encoder(img)
+    
+    def forward(self, observation, prev_action=None, prev_reward=None):
+        #TODO Augmentation the img?
+        pred_ps = []
+        pred_reward = []
+        pred_latents = []
+        
+        input_obs = observation[0].flatten(1, 2)
+        #Berechne Latent Representation mit Online Encoder
+        latent = self.o_encoder_forward(input_obs)
+        pred_latents.append(latent)
+        #Berechne Q Value von Q-Learning Head
+        pred_ps.append(self.head_forward(latent))
+        
+        #Falls k > 0
+        if self.jumps > 0:
+            #Berechne zukünftige Reward von Prediction 
+            pred_reward.append(self.dynamics_model.reward_predictor(pred_latents[0]))
+            
+            #für jeden k-Step in die Zukunft
+            for j in range(1, self.jumps + 1):
+                #predicte mit Transition Model die nächsten Latent State und Rewards
+                latent, pred_rew = self.step(latent, prev_action[j])
+                pred_rew = pred_rew[:observation.shape[1]]
+                pred_latents.append(latent)
+                pred_reward.append(pred_rew)
+        
+        #nicht ganz klar was model_rl sein soll?
+        if self.model_rl > 0:
+            for i in range(1, len(pred_latents)):
+                pred_ps.append(self.head_forward(pred_latents[i]))
+              
+
+        pred_latents = torch.stack(pred_latents, 1)
+        latents = pred_latents[:observation.shape[1]].flatten(0, 1)  # batch*jumps, *
+        neg_latents = pred_latents[observation.shape[1]:].flatten(0, 1)
+        #concat die jetzige latent representation und predited latent representation von Transition model
+        latents = torch.cat([latents, neg_latents], 0)
+        target_images = observation[self.time_offset:self.jumps + self.time_offset+1].transpose(0, 1).flatten(2, 3)
+        #augmentation??
+        #target_images = self.transform(target_images, True) 
+        target_images = target_images[..., -1:, :, :]
+        with torch.no_grad():
+            #Berechne Latent representation von observation aus dem replay buffer mit target encoder
+            target_latents = self.target_encoder(target_images.flatten(0, 1))
+              
+        
+        online_latents = latents.flatten(-2, -1).permute(2, 0, 1)
+        target_latents = target_latents.flatten(-2, -1).permute(2, 0, 1)
+        #berechne latent representation mit online projection
+        online_latents = self.online_projection(online_latents)
+        
+        with torch.no_grad():
+            #berechne latent representation mit target projection
+            target_latents = self.target_projection(target_latents)
+
+        # online_latents = online_latents.view(-1,
+        #                                    observation.shape[1],
+        #                                    self.jumps+1,
+        #                                    online_latents.shape[-1]).transpose(1, 2)
+        # target_latents = target_latents.view(-1,
+        #                                    observation.shape[1],
+        #                                    self.jumps+1,
+        #                                    target_latents.shape[-1]).transpose(1, 2)
+
+        #normalisiere
+        online_latents = F.normalize(online_latents.float(), p=2., dim=-1, eps=1e-3)
+        target_latents = F.normalize(target_latents.float(), p=2., dim=-1, eps=1e-3)
+        
+        # Gradients of norrmalized L2 loss and cosine similiarity are proportional.
+        # See: https://stats.stackexchange.com/a/146279 
+        sprloss = F.mse_loss(online_latents, target_latents, reduction="none").sum(-1).mean(0)
+
+        return pred_ps, pred_reward, sprloss
+    
+    #predicte die nächsten state und reward
+    def step(self, state, action):
+        next_state, reward = self.transition(state, action)
+        return next_state, reward
+        
+class MLPQHead(nn.Module):
     def __init__(self, input_channel, output_size, hidden_size, pixels):
-        super(MLPOnlineProjectionHead, self).__init__()
+        super(MLPQHead, self).__init__()
         
         self.input = nn.Linear(input_channel * pixels, hidden_size)
         self.output = nn.Linear(hidden_size, output_size)
@@ -140,7 +240,8 @@ class RewardPredictor(nn.Module):
                   nn.Flatten(-3, -1),
                   nn.Linear(pixels*hidden_size, 256),
                   nn.ReLU(),
-                  nn.Linear(256, limit*2 + 1)]
+                  #nn.Linear(256, limit*2 + 1)]
+                  nn.Linear(256, limit)]
         self.network = nn.Sequential(*layers)
         self.train()
 
