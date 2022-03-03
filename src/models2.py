3f26a8316e6ece3c69405e736f28e187bf491efa
Tri Hiep Bui <hiepbui945@gmail.com>
2022-03-03 22:03:08 +0100

    tau parameter

--- src/models2.py
+++ src/models2.py
@@ -46,7 +46,7 @@ class OnlineEncoder(nn.Module):
 
 
 class DuelingDDQN(nn.Module):
-    def __init__(self, inChannels, dimHid, dimOut, noAugmentation):
+    def __init__(self, inChannels, dimHid, dimOut, noAugmentation, Tau):
         "docstring"
         super(DuelingDDQN, self).__init__()
 
