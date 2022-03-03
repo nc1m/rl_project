16761dd338778c35ee8e52d1ba5d4c062c11535d
nc1m <userw@debian-desktop>
2022-03-03 22:06:39 +0100

    removed tau

--- src/models2.py
+++ src/models2.py
@@ -47,7 +47,7 @@ class OnlineEncoder(nn.Module):
 
 
 class DuelingDDQN(nn.Module):
-    def __init__(self, inChannels, dimHid, dimOut, noAugmentation, Tau):
+    def __init__(self, inChannels, dimHid, dimOut, noAugmentation):
         "docstring"
         super(DuelingDDQN, self).__init__()
         print(f'dimHid: {dimHid}')
